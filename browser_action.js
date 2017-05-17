8420d8eeefaa9a7ccfc4bab5f0869af100b5e33a
Park9eon <quiet9.noise@gmail.com>
2017-05-17 20:03:53 +0900

    first commit

---
+++ browser_action.js
@@ -0,0 +1,10 @@
+(function(){
+	var checkbox = document.getElementById('checkbox');
+	chrome.storage.sync.get(function(data) {
+		checkbox.checked = data.visibility;
+	});
+	checkbox.addEventListener("change", function() {
+		chrome.storage.sync.set({visibility: checkbox.checked}, function() {
+		});
+	});
+})();
---
+++ content.js
@@ -0,0 +1,27 @@
+(function () {
+	function update(visibility) {
+		if (visibility) {
+			unloadCSS();
+		} else {
+			loadCSS('content')
+		}
+	}
+	function loadCSS(file) {
+		var link = document.createElement("link");
+		link.href = chrome.extension.getURL(file + '.css');
+		link.id = "yt-blind-style";
+		link.type = "text/css";
+		link.rel = "stylesheet";
+		document.getElementsByTagName("html")[0].appendChild(link);
+	}
+	function unloadCSS() {
+		var cssNode = document.getElementById("yt-blind-style");
+		cssNode && cssNode.parentNode.removeChild(cssNode);
+	}
+	chrome.storage.sync.get(function(data) {
+		update(data.visibility);
+	});
+	chrome.storage.onChanged.addListener(function(changes, namespace) {
+		update(changes.visibility.newValue);
+	});
+})();
